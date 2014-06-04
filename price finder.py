72be443d847ba3f3cb80e2eb54a53b9888fa8d12
chubbychubs <mail.wyrm@gmail.com>
2014-06-04 20:43:20 +0800

    Create a main function, which now takes in a user given URL, and converts that into a string containing the HTML code of the given URL. Create the function convert_to_HTML, which is implemented in the main function.

--- price finder.py
+++ price finder.py
@@ -11,4 +11,27 @@ Created on Wed Jun 04 18:09:07 2014
 
 @author: chubbychubs
 """
+import urllib2
+import BeautifulSoup
 
+def main():
+    #prompt user for the URL of the web page
+    print('test')
+    url = raw_input('Please indicate the URL of the webpage that lists the products and their prices')
+    #converts it into HTML
+    html = convert_to_HTML(url)
+
+
+def convert_to_HTML(url):
+    """This function takes in the URL of a web page, and then returns a string 
+    containing the HTML of the web page"""    
+    #create the HTML request to the website
+    request = urllib2.Request(url)
+    #create the response object for the HTML request
+    response = urllib2.urlopen(request)
+    #take the HTML from the response object
+    html = response.read()
+    #convert it into better formatting using BeautifulSoup
+    soup = BeautifulSoup.BeautifulSoup(html)
+    return soup
+    
