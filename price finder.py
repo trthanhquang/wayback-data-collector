ea51f5135384315f0b2a558810e797b3d7f70e7c
chubbychubs <mail.wyrm@gmail.com>
2014-06-04 18:31:27 +0800

    Created a new python file called price finder.py and created the description of what this program does

---
+++ price finder.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#This program takes in a URL as its input, takes the HTML from the webpage, 
+#searches for a list of products and compiles the name and prices of the products
+#It does this in two ways:
+#Number 1 - It will search for a table header tag <th> and look for the strings
+#"product" and "price", and store the data
+#Number 2 - If such a table does not exist, then the program will look for
+#tags/attributes
+"""
+Created on Wed Jun 04 18:09:07 2014
+
+@author: chubbychubs
+"""
+
