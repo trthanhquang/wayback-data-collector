cc42db4235b5e38c7e006a53b5bd765ee1115c41
bilun167 <bilun167@gmail.com>
2014-06-08 23:19:19 +0800

    Extract meaningful text and Rank word frequency

--- wayback_library/meaningfulText+wordFrequency.py
+++ wayback_library/meaningfulText+wordFrequency.py
@@ -1,10 +1,9 @@
+import collections
 import urllib2
-from bs4 import BeautifulSoup
+from BeautifulSoup import BeautifulSoup
 import nltk
 from nltk import FreqDist
 import re, os
-url="http://www.3delite.hu/"
-page =urllib2.urlopen(url)
 
 html_data = open("pageSource.txt",'r').read()
 soup = BeautifulSoup(html_data)
@@ -20,15 +19,23 @@ f.close()
 
 #words frequency distribution & filter out transition words
 para = open('collectParagraph.txt', 'r').read()
+#filter out punctuation
 para1 = re.sub('[^A-Za-z0-9]+', ' ', str(para))
+para1 = para1.lower()
+#para1 = re.sub('[a-zA-Z0-9]{2,}', '', str(para))
 
 #1 replace all transtion words with blank
-transitionword = {"and": "", "for": "","a": "","an": ""}
-
+#transitionword = {"and": "", "for": "","a": "","an": ""}
+transitionword = collections.defaultdict()
+with open('linkingWords.txt') as f:
+    lines = [l.strip() for l in f if l.strip()]
+for word in lines:
+    transitionword[word] = ("");
 
 transitionword = dict((re.escape(k), v) for k, v in transitionword.iteritems())
-pattern = re.compile("|".join(transitionword.keys()))
-final = pattern.sub(lambda m: transitionword[re.escape(m.group(0))], str(para1))
+pattern = "|".join("\\b%s\\b"%re.escape(p) for p in transitionword)
+final = re.sub(pattern, lambda m: transitionword.get(m), str(para1))
+#print pattern
 
 #2 words frequency distribution top 100
 tokens = nltk.word_tokenize(final)
