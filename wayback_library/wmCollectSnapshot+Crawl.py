3e78744cfa559a3eeff45d2ce11c5146fe6e09bf
bilun167 <bilun167@gmail.com>
2014-06-12 15:46:59 +0800

    refactor

--- wayback_library/wmCollectSnapshot+Crawl.py
+++ wayback_library/wmCollectSnapshot+Crawl.py
@@ -13,62 +13,57 @@ phantomJSpath = 'C:\phantomjs-1.9.7-windows\phantomjs.exe'
 
 db = MySQLdb.connect(host = "localhost", user = "root", passwd = "", db = "wbm")
 cur = db.cursor()
-cur.execute("select `itemID`, `Publisher URL Search` from item");
-
+cur.execute("select `itemID`, `Publisher URL Search` from item order by itemID ASC");
 for row in cur.fetchmany(2):
 
     '''get date and link of waybackmachine and put in link[tag]'''
 
     itemId = row[0];
-    def main() :
-        query = row[1];
-        year = "2014"
+    query = row[1];
+    year = "2014"
 
-        w= WB()
-        (status,links)=w.get_wayback_timetable_links(query,year)
+    w= WB()
+    (status,links)=w.get_wayback_timetable_links(query,year)
+
+    for tag in links:
+        print tag,links[tag]
+        date = tag.encode('utf8')
+        #print date
+        '''from each link wm crawl html by using phantomjs'''
+
+        linkwm = links[tag]
+        if linkwm =="":
+            print 'empty link!'
+            continue
+        driver = webdriver.PhantomJS(executable_path=phantomJSpath)
+        driver.get(linkwm) 
+        driver.quit()
+
+        crawl_data = driver.page_source.encode('utf8')
+
+
+        '''from craw_data convert to meaningful text'''
+        soup = BeautifulSoup(crawl_data)
+        text = str(soup)
+        meaningfulText = nltk.clean_html(text)
+        meaningfulText = os.linesep.join([s for s in meaningfulText.split('\n') if s.strip() !=''])
+
+        '''write on MySQL craw_data and meaningful text'''
         
-        for tag in links:
-            print tag,links[tag]
-            date = tag.encode('utf8')
-            print date
-            '''from each link wm crawl html by using phantomjs'''
-            driver = webdriver.PhantomJS(executable_path=phantomJSpath)
-            linkwm = links[tag]
-            if linkwm =="":
-                print 'empty link!'
-                continue
-            driver.get(linkwm)
-            #driver.get("http://www.netscantools.com")
-            crawl_data = driver.page_source.encode('utf8')
-            driver.quit()
-    
+        crawl_data = MySQLdb.escape_string(crawl_data)
+        #print crawl_data
+        meaningfulText = MySQLdb.escape_string(meaningfulText)
+        #print meaningfulText
 
-            '''from craw_data convert to meaningful text'''
-            soup = BeautifulSoup(crawl_data)
-            text = str(soup)
-            meaningfulText = nltk.clean_html(text)
-            meaningfulText = os.linesep.join([s for s in meaningfulText.split('\n') if s.strip() !=''])
-
-            '''write on MySQL craw_data and meaningful text'''
-            
-            crawl_data = MySQLdb.escape_string(crawl_data)
-            #print crawl_data
-            meaningfulText = MySQLdb.escape_string(meaningfulText)
-            #print meaningfulText
-    
-            query = """insert into snapshot_2014(itemID, date, crawl_data, meaningfulText) values(%s, STR_TO_DATE(\"%s\", \"%%b-%%e-%%Y\"), \"%s\", \"%s\");""" % (itemId, date, crawl_data, meaningfulText)
-            #print query
-            cur.execute(query)
-            db.commit()
-            #f = open('query.txt', 'w')
-            #for line in query.split('\n') :
-            #    if line.strip() != '' :
-            #        f.write(line + str('\n'))
-            #f.close()
-            #return
-
-    if __name__ == "__main__":
-        main()
-
+        query = """insert into snapshot_2014(itemID, date, crawl_data, meaningfulText) values(%s, STR_TO_DATE(\"%s\", \"%%b-%%e-%%Y\"), \"%s\", \"%s\");""" % (itemId, date, crawl_data, meaningfulText)
+        #print query
+        cur.execute(query)
+        db.commit()
+        #f = open('query.txt', 'w')
+        #for line in query.split('\n') :
+        #    if line.strip() != '' :
+        #        f.write(line + str('\n'))
+        #f.close()
+        #return   
     
    
