a62bdb16b42d5d4cabcded6e968880230f65ccc7
bilun167 <bilun167@gmail.com>
2014-06-13 14:52:27 +0800

    renaming

--- wayback_library/wmCollectSnapshot+Crawl.py
+++ wayback_library/wmCollectSnapshot+Crawl.py
@@ -1,5 +1,7 @@
-import collections
 import urllib2
+from BeautifulSoup import BeautifulSoup
+import re
+import collections
 import nltk
 import re,os
 from selenium import webdriver
@@ -8,40 +10,51 @@ import urllib
 from bs4 import BeautifulSoup as BS
 from BeautifulSoup import BeautifulSoup
 from wm2 import WaybackMachine as WB
+from collections import OrderedDict
 
 phantomJSpath = 'C:\phantomjs-1.9.7-windows\phantomjs.exe'
-
 db = MySQLdb.connect(host = "localhost", user = "root", passwd = "", db = "wbm")
 cur = db.cursor()
+
+wm = "http://web.archive.org"
+wmstart = "http://web.archive.org/web/"
+#urlAddr = "www.netscantools.com"
+#urlAddr = "seriousbit.com"
+
+year = "2014"
+
 cur.execute("select `itemID`, `Publisher URL Search` from item order by itemID ASC");
-for row in cur.fetchmany(2):
+#rows = cur.fetchmany(2)
+for row in cur:
+    itemId = row[0]
+    urlAddr = row[1]
+    print "%s: %s\n" % (itemId, urlAddr)
+    if not urlAddr.startswith("http"):
+        urlAddr2 = "http://" + urlAddr #if the url does not start with 'http'...
+    else:
+        urlAddr2 = urlAddr
 
-    '''get date and link of waybackmachine and put in link[tag]'''
 
-    itemId = row[0];
-    query = row[1];
-    year = "2014"
+    page = urllib2.urlopen(wmstart + year + "0000000000*/" + urlAddr2)
+    soup = BeautifulSoup(page.read())
+    links = soup.findAll("a")
 
-    w= WB()
-    (status,links)=w.get_wayback_timetable_links(query,year)
+    link_list = []
+    for link in links:
+        if re.match("(.*)%s(.*)" % year, str(link), re.I):
+            if not "*" in str(link):
+                linkwm = wm + link["href"]
+                link_list.append(linkwm)
 
-    for tag in links:
-        print tag,links[tag]
-        date = tag.encode('utf8')
-        #print date
-        '''from each link wm crawl html by using phantomjs'''
-
-        linkwm = links[tag]
-        if linkwm =="":
-            print 'empty link!'
-            continue
+    for final_link in list(set(link_list)):
+        
+        date = final_link[27:35].encode('utf8')
+        print ("\t%s\n") % final_link
         driver = webdriver.PhantomJS(executable_path=phantomJSpath)
         driver.get(linkwm)
         crawl_data = driver.page_source.encode('utf8')
         driver.quit()
 
-
-        '''from craw_data convert to meaningful text'''
         soup = BeautifulSoup(crawl_data)
         text = str(soup)
         meaningfulText = nltk.clean_html(text)
@@ -56,13 +69,8 @@ for row in cur.fetchmany(2):
 
         query = """insert into snapshot_2014(itemID, date, crawl_data, meaningfulText) values(%s, STR_TO_DATE(\"%s\", \"%%b-%%e-%%Y\"), \"%s\", \"%s\");""" % (itemId, date, crawl_data, meaningfulText)
         #print query
-        cur.execute(query)
-        db.commit()
-        #f = open('query.txt', 'w')
-        #for line in query.split('\n') :
-        #    if line.strip() != '' :
-        #        f.write(line + str('\n'))
-        #f.close()
-        #return   
+        #cur.execute(query)
+        #db.commit()
     
-   
+
+    
