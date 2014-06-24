b262a139cea6c55f34eaa64015d53a92d8c030a2
trthanhquang <trthanhquang@gmail.com>
2014-06-24 23:27:05 +0800

    multi-thread binary search

--- louis-html-analyzer/binarySearch.py
+++ louis-html-analyzer/binarySearch.py
@@ -1,51 +1,80 @@
 from bisect import bisect_left
-
+from Queue import *
 import random
 
-# randomSeed = 123456789
-randomSeed = random.random()*123456789
+randomSeed = 123456789
+# randomSeed = random.random()*123456789
 
 noVersion = 10
 listSize = 200
 
-itemList = []
-visitedList = []
+itemList = None #url list
+crawledlist = None #downloaded page
+downloadCount = None
 
 def init():
-	global itemList,visitedList
+	global itemList,crawledlist, downloadCount
 	newList = []
-	visitedList = []
+	crawledlist = []
+	downloadCount = 0
 	random.seed(randomSeed)
 	for i in range(listSize):
 		newItem = int(random.random()*noVersion)
 		newList.append(newItem)
-		visitedList.append(0)
+		crawledlist.append(0)
 
 	itemList = sorted(newList)
-	
+
+def download(dlist):
+	global downloadCount
+	download = False
+	for i in dlist:
+		if(crawledlist[i]==0):
+			crawledlist[i]=1
+			download = True
+	if download:
+		downloadCount = downloadCount+1
+
+def getDivideIndex(lo,hi,level):
+	indexList = []
+	if level == 0:
+		return indexList
+	mid = lo + (hi-lo)/2
+	indexList.append(mid)
+
+	indexList.extend(getDivideIndex(lo,mid,level-1))
+	indexList.extend(getDivideIndex(mid,hi,level-1))
+	return set(indexList)
+
 def binarySearchDiff(lo, hi):
+	digLevel = 6
+	download(getDivideIndex(lo,hi,digLevel))
+
 	searchItem = itemList[lo]
-	visitedList[lo] = 1
-	
 	if(itemList[hi]==searchItem):
-		visitedList[hi] = 1
 		return -1
 
 	while(lo<=hi):
+		if(digLevel == 0):
+			print 'additional dig at (%s,%s)'%(lo,hi)
+			digLevel = 6
+			download(getDivideIndex(lo,hi,digLevel))
+
 		mid = lo+(hi-lo)/2
 		# print 'lo= %s, hi=%s, mid=%s'%(lo,hi,mid)
-		visitedList[mid]=1
 		if(itemList[mid]!=searchItem):
 			hi = mid -1
 		else:
 			lo = mid +1
+
+		digLevel = digLevel -1
 	return lo
 
 def refineSearchRange(lo,hi):
 	searchItem = itemList[lo]
 
 	for i in range(lo,hi,1):
-		if(visitedList[i]==1):
+		if(crawledlist[i]==1):
 			if(itemList[i]==searchItem):
 				lo = i
 			else:
@@ -54,7 +83,6 @@ def refineSearchRange(lo,hi):
 
 	# print 'update range from %s to %s'%(lo,hi)
 	return (lo,hi)
-
 def normalSearch():
 	init()
 	# print itemList
@@ -68,14 +96,9 @@ def normalSearch():
 		lo = binarySearchDiff(lo,hi)
 		# print 'index=%s, item=%s'%(lo,itemList[lo])
 	
-	visited = sum(visitedList)
+	visited = sum(crawledlist)
 	percentage = 100.0*visited/len(itemList)
-	print 'normalSearch: visited = {0}, percentage = {1:.3g}%'.format(visited,percentage)
-
-def resetVisitedList():
-	for i in range(len(visitedList)):
-		visitedList[i]=0
-
+	print 'normalSearch: download {2} times ({0} snapshots downloaded, {1:.3g}%)'.format(visited,percentage,downloadCount)
 def advanceSearch():
 	init()
 	# print itemList
@@ -86,12 +109,12 @@ def advanceSearch():
 	while(lo!=-1):
 		hi = endIndex
 		(lo,hi)= refineSearchRange(lo,hi)
-		lo = binarySearchDiff(lo,hi)
-		# print 'index=%s, item=%s'%(lo,itemList[lo])
+		lo= binarySearchDiff(lo,hi)
+		print 'index=%s, item=%s'%(lo,itemList[lo])
 	
-	visited = sum(visitedList)
+	visited = sum(crawledlist)
 	percentage = 100.0*visited/len(itemList)
-	print 'advanceSearch: visited = {0}, percentage = {1:.3g}%'.format(visited,percentage)
+	print 'advanceSearch: download {2} times ({0} snapshots downloaded, {1:.3g}%)'.format(visited,percentage,downloadCount)
 
 
 def main():
@@ -99,7 +122,7 @@ def main():
 	noVersion = 20
 	listSize = 2000
 
-	normalSearch()
+	# normalSearch()
 	advanceSearch()
 
 if __name__ == '__main__':
