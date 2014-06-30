72040c926bedc76b4c9b115d6ba9bb5816640807
bilun167 <bilun167@gmail.com>
2014-06-30 21:13:32 +0800

    Minor
    Conflicts:
    	crawler/database.py

--- crawler/database.py
+++ crawler/database.py
@@ -7,12 +7,12 @@ class database(object):
     def __init__(self, hostName="localhost", userName="root",
                  password="", database="crawling"):
         self.db = None
-        while self.db is None
+        while self.db is None:
             self.db = self.__getConnection(hostName, userName, password, database)
             
         self.db.autocommit(True)
         self.cur = self.db.cursor()
-
+        
     def __del__(self):
         self.cur.close()
         self.db.close()
